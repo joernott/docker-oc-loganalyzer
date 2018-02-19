f34a41c8d0b4df08902af8bdd80e93470aacf931
Joern Ott <joern.ott@ott-consult.de>
2018-02-19 09:44:40 +0100

    Fix typo

--- src/tmp/install/oc-loganalyzer.sh
+++ src/tmp/install/oc-loganalyzer.sh
@@ -10,6 +10,6 @@ echo "${LOGANALYZER_MD5} *loganalyzer.tar.gz" >loganalyzer.md5
 md5sum -c loganalyzer.md5
 tar -xzf loganalyzer.tar.gz
 rm -rf /var/www/html
-mv loganalyzer-${PHPMYADMIN_VERSION}/src /var/www/html
+mv loganalyzer-${LOGANALYZER_VERSION}/src /var/www/html
 rm -rf loganalyzer.tar.gz loganalyzer.md5 loganalyzer-${PHPMYADMIN_VERSION}
 cleanup
